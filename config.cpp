ff8017a75cd53cfdf65f8fc643164c569f3baf03
Jeff Day <jeffday@gmail.com>
2023-11-02 17:39:46 -0500

    first commit, allow TWM Five Seven to use the 5.7x28mm ammo from Ammunition Expansion

---
+++ config.cpp
@@ -0,0 +1,86 @@
+
+class CfgPatches
+{
+	class AmmoExpansionCompatTWM
+	{
+		requiredVersion = 0.1;
+		requiredAddons[] = {
+			"DZ_Data",
+			"DZ_Pistols",
+			"DZ_Weapons_Firearms",
+			"DZ_Weapons_Magazines",
+			"Ammo_AE57x28",
+			"TWM_FN57"
+		};
+	}
+};
+class CfgMods
+{
+	class AmmoExpansionCompatTWM
+	{
+		dir = "AmmoExpansionCompatTWM";
+		picture = "";
+		action = "";
+		hideName = 1;
+		hidePicture = 1;
+		name = "Ammunition Expansion Compatibility Tech's Weapon Mod";
+		author = "angrystoma";
+		authorID = "0";
+		version = "1.0";
+		extra = 0;
+		type = "mod";
+		inputs = "";
+		dependencies[]=
+		{
+			"Game",
+			"World",
+			"Mission"
+		};
+		class defs
+		{
+			class missionScriptModule
+			{
+				value="";
+				files[]= {"AmmoExpansionCompatTWM\Scripts\5_Mission"};
+			};
+			class worldScriptModule
+			{
+				value = "";
+				files[] = {"AmmoExpansionCompatTWM\Scripts\4_World"};
+			};
+			class gameScriptModule
+			{
+				value = "";
+				files[] = {"AmmoExpansionCompatTWM\Scripts\3_Game"};
+			};
+		};
+	};
+};
+
+class cfgWeapons
+{
+
+	class Colt1911_Base;
+	class TWM_FN57_Base: Colt1911_Base
+	{
+		chamberableFrom[]+=
+		{
+			"Ammo_AE57x28"
+		};
+	}
+};
+
+class CfgMagazines
+{
+	class DefaultMagazine;
+	class Magazine_Base;
+
+	class TWM_Mag_FN57: Magazine_Base
+	{
+		ammo="TWM_Bullet_57x28";
+		ammoItems[]+=
+		{
+			"Ammo_AE57x28"
+		};
+	}
+}
